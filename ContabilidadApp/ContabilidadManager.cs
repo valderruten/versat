9e8e90ca88f3518245cccdb84f34f8be367d1966
Carlos Andrés V <46254285+valderruten@users.noreply.github.com>
2023-09-24 22:00:07 -0500

    Agregar archivos de proyecto.

---
+++ ContabilidadApp/ContabilidadManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public class ContabilidadManager
+{
+    private string connectionString;
+
+    public ContabilidadManager(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public decimal CalcularMargenBeneficioNeto(DateTime fechaInicio, DateTime fechaFin)
+    {
+        decimal margenBeneficioNeto = 0;
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand cmd = new SqlCommand("CalcularMargenBeneficioNeto", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FechaInicio", fechaInicio);
+                cmd.Parameters.AddWithValue("@FechaFin", fechaFin);
+
+                object result = cmd.ExecuteScalar();
+                if (result != null && result != DBNull.Value)
+                {
+                    margenBeneficioNeto = Convert.ToDecimal(result);
+                }
+            }
+        }
+
+        return margenBeneficioNeto;
+    }
+
+    public DataTable CalcularBalanceGeneral(DateTime fechaInicio, DateTime fechaFin)
+    {
+        DataTable balanceGeneral = new DataTable();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand cmd = new SqlCommand("CalcularBalanceGeneral", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FechaInicio", fechaInicio);
+                cmd.Parameters.AddWithValue("@FechaFin", fechaFin);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(balanceGeneral);
+            }
+        }
+
+        return balanceGeneral;
+    }
+
+    public DataTable CalcularEstadoResultados(DateTime fechaInicio, DateTime fechaFin)
+    {
+        DataTable estadoResultados = new DataTable();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand cmd = new SqlCommand("CalcularEstadoResultados", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FechaInicio", fechaInicio);
+                cmd.Parameters.AddWithValue("@FechaFin", fechaFin);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(estadoResultados);
+            }
+        }
+
+        return estadoResultados;
+    }
+
+    public decimal CalcularIndiceLiquidezCorriente(DateTime fechaInicio, DateTime fechaFin)
+    {
+        decimal indiceLiquidezCorriente = 0;
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand cmd = new SqlCommand("CalcularIndiceLiquidezCorriente", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FechaInicio", fechaInicio);
+                cmd.Parameters.AddWithValue("@FechaFin", fechaFin);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        
+                        indiceLiquidezCorriente = reader.GetDecimal(0);
+                    }
+                }
+            }
+        }
+
+        // Formatear el resultado como decimal con dos decimales
+        indiceLiquidezCorriente = Math.Round(indiceLiquidezCorriente, 2);
+
+        return indiceLiquidezCorriente;
+    }
+
+}
+
+
+
+
+
+
---
+++ ContabilidadApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ContabilidadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-OEJ1QD3\\SQLEXPRESS01;Database=ContabilidadEmpresa;Trusted_Connection=True;";
+
+            Console.WriteLine("Ingrese la fecha de inicio (yyyy-MM-dd):");
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime fechaInicio))
+            {
+                Console.WriteLine("Ingrese la fecha de fin (yyyy-MM-dd):");
+                if (DateTime.TryParse(Console.ReadLine(), out DateTime fechaFin))
+                {
+                    ContabilidadManager contabilidadManager = new ContabilidadManager(connectionString);
+
+
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    // Calcular y mostrar el balance general
+                    DataTable balanceGeneral = contabilidadManager.CalcularBalanceGeneral(fechaInicio, fechaFin);
+                    Console.WriteLine("Balance General:");
+                    foreach (DataRow row in balanceGeneral.Rows)
+                    {
+                        Console.WriteLine($"{row["Categoria"]}: {row["Monto"]}");
+                    }
+                    Console.WriteLine();
+
+                    // Calcular y mostrar el estado de resultados
+                    DataTable estadoResultados = contabilidadManager.CalcularEstadoResultados(fechaInicio, fechaFin);
+                    Console.WriteLine("Estado de Resultados:");
+                    foreach (DataRow row in estadoResultados.Rows)
+                    {
+                        Console.WriteLine($"{row["Categoria"]}: {row["Valor"]}");
+                    }
+                    Console.WriteLine();
+
+                    // Calcular y mostrar el índice de liquidez corriente
+                    decimal indiceLiquidezCorriente = contabilidadManager.CalcularIndiceLiquidezCorriente(fechaInicio, fechaFin);
+                    Console.WriteLine($"Análisis de Liquidez: {indiceLiquidezCorriente}");
+                    Console.WriteLine();
+
+                    // Calcular y mostrar el Análisis de Rentabilidad
+                    decimal margenBeneficioNeto = contabilidadManager.CalcularMargenBeneficioNeto(fechaInicio, fechaFin);
+                    Console.WriteLine($"Análisis de Rentabilidad: {margenBeneficioNeto}%");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Fecha de fin inválida. Asegúrese de ingresar la fecha en formato yyyy-MM-dd.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fecha de inicio inválida. Asegúrese de ingresar la fecha en formato yyyy-MM-dd.");
+            }
+
+            Console.WriteLine("Presione Enter para salir...");
+            Console.ReadLine();
+        }
+    }
+}
